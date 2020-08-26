c369f2bd2e80bf7287f074f26822e33ca826e08b
linghuam <1551827898@qq.com>
2020-08-26 15:01:30 +0800

    按需

--- demo_17_SystemJs/main.js
+++ demo_17_SystemJs/main.js
@@ -1,10 +1,14 @@
 import $ from 'jquery'
 import * as lodash from 'lodash'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
 
 export default {
   initChart(ref, config) {
     const nConfig = lodash.cloneDeep(config);
     const $comp = $(ref);
+    echarts.init(ref);
     console.log('initchart', lodash, $);
   }
 }
